92a43a6c0ee43086a18c96b14592b5edbc7509c6
hakim2999 <49701397+hakim2999@users.noreply.github.com>
2024-01-22 13:36:40 +0200

    tran

--- public/main-transpiled.js
+++ public/main-transpiled.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+// import App from './App.jsx';
+// import './index.css';
+import App from './src/App.jsx';
+
+import './src/index.css';
 import { BrowserRouter } from 'react-router-dom';
 // import reportWebVitals from '../src/reportWebVitals.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
