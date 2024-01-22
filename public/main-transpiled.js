cd9d1d2d1d2d5ac284d27ec8f2fa3ff03992ea33
hakim2999 <49701397+hakim2999@users.noreply.github.com>
2024-01-22 13:23:13 +0200

    update

---
+++ public/main-transpiled.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+// import reportWebVitals from '../src/reportWebVitals.js';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( /*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(BrowserRouter, null, /*#__PURE__*/React.createElement(App, null))));
+// reportWebVitals();
